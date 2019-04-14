2b251a094e09c024e5b9d45ff7f0104f6713c4dd
lakinduakash <lakinduakash@gmail.com>
2019-04-15 04:14:32 +0530

    Move config related definitions to read_config.h

--- src/ui/h_prop.h
+++ src/ui/h_prop.h
@@ -6,44 +6,6 @@
 #define WIHOTSPOT_H_PROP_H
 
 
-#define CHANNEL          "CHANNEL"
-#define GATEWAY          "GATEWAY"
-#define WPA_VERSION      "WPA_VERSION"
-#define ETC_HOSTS        "ETC_HOSTS"
-#define DHCP_DNS         "DHCP_DNS"
-#define NO_DNS           "NO_DNS"
-#define NO_DNSMASQ       "NO_DNSMASQ"
-#define HIDDEN           "HIDDEN"
-#define MAC_FILTER       "MAC_FILTER"
-#define MAC_FILTER_ACCEP "MAC_FILTER_ACCEPT"
-#define ISOLATE_CLIENTS  "ISOLATE_CLIENTS"
-#define SHARE_METHOD     "SHARE_METHOD"
-#define IEEE80211N       "IEEE80211N"
-#define IEEE80211AC      "IEEE80211AC"
-#define HT_CAPAB         "HT_CAPAB"
-#define VHT_CAPAB        "VHT_CAPAB"
-#define DRIVER           "DRIVER"
-#define NO_VIRT          "NO_VIRT"
-#define COUNTRY          "COUNTRY"
-#define FREQ_BAND        "FREQ_BAND"
-#define NEW_MACADDR      "NEW_MACADDR"
-#define DAEMONIZE        "DAEMONIZE"
-#define NO_HAVEGED       "NO_HAVEGED"
-#define WIFI_IFACE       "WIFI_IFACE"
-#define INTERNET_IFACE   "INTERNET_IFACE"
-#define SSID             "SSID"
-#define PASSPHRASE       "PASSPHRASE"
-#define USE_PSK          "USE_PSK"
-
-
-
-#define CONFIG_FILE_NAME ".wh.config"
-
-typedef struct{
-    char *ssid;
-    char *pass;
-} ConfigValues;
-
 static int parse_output(const char *);
 
 const char *build_wh_start_command(char *, char *, char *, char *);
