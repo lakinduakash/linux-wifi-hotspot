e3e3418a11d255afbbade48627120454c4c56d7b
lakinduakash <lakinduakash@gmail.com>
2019-04-16 02:48:16 +0530

    Add util.c

---
+++ src/ui/util.h
@@ -0,0 +1,10 @@
+//
+// Created by lakinduakash on 16/04/19.
+//
+
+#ifndef WIHOTSPOT_UTIL_H
+#define WIHOTSPOT_UTIL_H
+
+int find_str(char *find, const char **array, int length);
+
+#endif //WIHOTSPOT_UTIL_H
