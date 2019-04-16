b19924c6c9febcc3075bd5957f6c5cee3d7867c7
lakinduakash <lakinduakash@gmail.com>
2019-04-16 09:42:40 +0530

    Add random string generator function

--- src/ui/util.c
+++ src/ui/util.c
@@ -3,6 +3,8 @@
 //
 
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 #include "util.h"
 
 int find_str(char *find, const char **array, int length) {
@@ -17,4 +19,17 @@ int find_str(char *find, const char **array, int length) {
 
     return -1;
 
+}
+
+
+void rand_str(char *dest, size_t length) {
+    char charset[] = "0123456789"
+                     "abcdefghijklmnopqrstuvwxyz"
+                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    while (length-- > 0) {
+        size_t index = (size_t) ((double) rand() / RAND_MAX * (sizeof charset - 1));
+        *dest++ = charset[index];
+    }
+    *dest = '\0';
 }
