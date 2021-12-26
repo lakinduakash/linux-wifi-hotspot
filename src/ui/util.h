0ee524178f69b70b6b08c10f333c87f2c6282345
Lakindu Akash <lakinduakash@gmail.com>
2021-12-27 01:17:07 +0530

    Update util.h header

--- src/ui/util.h
+++ src/ui/util.h
@@ -35,5 +35,6 @@ int find_str(char *find, const char **array, int length);
 void rand_str(char *dest, size_t length);
 int isValidMacAddress(const char*);
 int isValidAcceptedMacs(const char*);
+int isValidIPaddress(const char*);
 
 #endif //WIHOTSPOT_UTIL_H
