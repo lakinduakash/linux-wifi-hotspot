0dd17db452f6c35b57832672ae8750b023757c6a
Komo <afiqquraisyzulkarnain@gmail.com>
2025-03-30 17:23:43 +0800

    fix(build-system): improve the build system

--- Makefile.cmake
+++ Makefile.cmake
@@ -2,14 +2,14 @@ all:
 	@echo "Run 'make install' for installation."
 	@echo "Run 'make uninstall' for uninstallation."
 	mkdir -p build
-	cd build && cmake -G "CodeBlocks - Unix Makefiles" ../src
+	cd build && cmake -DCMAKE_INSTALL_PREFIX=$(DESTDIR) ../src
 
 install:
 	@echo "Installing"
-	cd build && cmake -DCMAKE_INSTALL_PREFIX=$(DESTDIR) -G "CodeBlocks - Unix Makefiles" ../src
+	cd build && cmake -DCMAKE_INSTALL_PREFIX=$(DESTDIR) ../src
 	$(MAKE) -C src/scripts install
-	$(MAKE) -C build install_build
+	$(MAKE) -C build install
 
 uninstall:
 	$(MAKE) -C src/scripts uninstall
-	$(MAKE) -C build uninstall_build
+	$(MAKE) -C build uninstall
